48cb59f26135815c406d860484484c67159d0e44
dawjus <dawid.justyna@op.pl>
2023-02-18 00:09:23 +0100

    add choiceBox

--- App.py
+++ App.py
@@ -16,27 +16,40 @@ class App:
         self.root = Tk()
         self.canvas = Canvas(self.root, width=self.width*(sizecell+10), height=self.height*(sizecell+5))
         self.canvas.pack()
+        self.start=1
+        self.end =5
         self.draw_map()
         button = ttk.Button(
             self.root,
             text='Dijkstra',
-            command = self.but
+            command = self.dijkstra
         )
         button.place(x=130, y=10)
+        
+        self.choiceStart = IntVar()
+        self.choiceEnd = IntVar()
+        cbStart = ttk.Combobox(self.root, textvariable=self.choiceStart, values=self.choices)
+        cbStart.pack()
+        cbEnd = ttk.Combobox(self.root, textvariable=self.choiceEnd, values=self.choices)
+        cbEnd.pack()
+        
+       
 
-    def but(self):
+    def dijkstra(self):
         G = createGraph(self.matrix)
-        start = random.randint(0,len(G.vertex))
-        end = random.randint(0,len(G.vertex))
-        dist = dijkstra(G.vertex, G.vertex[start])
-        intek = end
+        dist = dijkstra(G.vertex, G.vertex[self.choiceStart.get()])
+        intek = self.choiceEnd.get()
         k = dist[G.vertex[intek].index]
         c = getShorthestPath(G, intek, k)
         for i in range(len(c)):
             self.matrix[c[i][0]][c[i][1]] = 4
+        print(self.choiceStart.get())
         self.draw_map()
 
     def draw_map(self):
+        
+        self.choices = []
+        countVertex=1
         for row in range(self.height):
             for col in range(self.width):
                 x1 = col*(self.sizecell+3)
@@ -50,8 +63,12 @@ class App:
                     counter = random.randint(0, len(self.alphabet)-1)
                     self.canvas.create_text((x1+x2)/2, (y1+y2)/2, text=self.alphabet[counter], font=("Arial", int(self.sizecell/1.4)))
                     self.alphabet = self.alphabet[:counter] + self.alphabet[counter+1:]
+                    self.choices.append(countVertex)
+                    countVertex+=1
                 elif value ==2:
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="green")
+                    self.choices.append(countVertex)
+                    countVertex+=1
                 elif value ==3:
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="blue")
                 elif value == 4:
@@ -64,9 +81,7 @@ class App:
         self.root.quit()
 
 
-#komenatrz
 map = Rm.RandomMap(10, 10)
 map = map.randomMap()
 my_map2 = App(10, map)
-my_map2.draw_map()
 my_map2.run()
